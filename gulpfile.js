88fdb998f7de6a9ca1e75bd642643594c4cce43a
htkz <htkzmo@gmail.com>
2020-04-10 00:45:56 -0400

    Try Gulp

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,14 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass');
 const browserSync = require('browser-sync');
+const del = require('del');
+const imagemin = require('gulp-imagemin');
+const uglify = require('gulp-uglify');
+const usemin = require('gulp-usemin');
+const rev = require('gulp-rev');
+const cleanCss = require('gulp-clean-css');
+const flatmap = require('gulp-flatmap');
+const htmlmin = require('gulp-htmlmin');
 
 gulp.task('sass', function () {
     return gulp
@@ -31,4 +39,50 @@ gulp.task('browser-sync', function () {
 
 gulp.task('default', ['browser-sync'], function () {
     gulp.start('sass:watch');
+});
+
+gulp.task('clean', () => {
+    return del(['dist']);
+});
+
+gulp.task('copyfonts', () => {
+    gulp.src('./node_modules/font-awesome/fonts/**/*.{ttf,wof,eof,svg}*').pipe(
+        gulp.dest('./dist/fonts')
+    );
+});
+
+gulp.task('imagemin', () => {
+    return gulp
+        .src('./img/*.{png, jpg, git}')
+        .pipe(
+            imagemin({
+                optimizationLevel: 3,
+                progressive: true,
+                interlaced: true,
+            })
+        )
+        .pipe(gulp.dest('dist/img'));
+});
+
+gulp.task('usemin', () => {
+    return gulp
+        .src('./*.html')
+        .pipe(
+            flatmap((stream, file) => {
+                return stream.pipe(
+                    usemin({
+                        css: [rev()],
+                        html: [() => htmlmin({ collapseWhitespace: true })],
+                        js: [uglify(), rev()],
+                        inlinejs: [uglify()],
+                        inlinecss: [cleanCss(), 'concat'],
+                    })
+                );
+            })
+        )
+        .pipe(gulp.dest('./dist/'));
+});
+
+gulp.task('build', ['clean'], () => {
+    gulp.start('copyfonts', 'imagemin', 'usemin');
 });
